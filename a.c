559d1e7e05350d03246a3d88e01baee4fcf6a683
zjl-lily <zjl_lily@163.com>
2022-01-19 17:21:22 +0800

    add a.c

---
+++ a.c
@@ -0,0 +1,14 @@
+// File Name: a.c
+// Author: xiaxiaosheng
+// Created Time: 2022年01月19日 星期三 17时20分42秒
+
+#include<stdio.h>
+#include<string.h>
+#include<stdlib.h>
+#include<time.h>
+#include<math.h>
+
+int main(){
+printf("nroot");
+return 0 ;
+}
